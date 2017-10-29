95b17b4bd8333c0a7dd08393010409aff4b81fe9
Tim Vaillancourt <tim@timvaillancourt.com>
2017-10-30 00:42:55 +0100

    Made NewMember() func, made AddMember set correct _id for new nodes

--- config/member.go
+++ config/member.go
@@ -1,17 +1,38 @@
 package config
 
 type Member struct {
-	Id          int          `bson:"_id"`
-	Host        string       `bson:"host"`
-	ArbiterOnly bool         `bson:"arbiterOnly"`
-	Hidden      bool         `bson:"hidden"`
-	Priority    int          `bson:"priority"`
-	Tags        *ReplsetTags `bson:"tags"`
-	SlaveDelay  int64        `bson:"slaveDelay"`
-	Votes       int          `bson:"votes"`
+	Id           int          `bson:"_id"`
+	Host         string       `bson:"host"`
+	ArbiterOnly  bool         `bson:"arbiterOnly"`
+	BuildIndexes bool         `bson:"buildIndexes"`
+	Hidden       bool         `bson:"hidden"`
+	Priority     int          `bson:"priority"`
+	Tags         *ReplsetTags `bson:"tags"`
+	SlaveDelay   int64        `bson:"slaveDelay"`
+	Votes        int          `bson:"votes"`
+}
+
+func NewMember(host string) *Member {
+	return &Member{
+		Host:         host,
+		BuildIndexes: true,
+		Priority:     1,
+		Votes:        1,
+	}
+}
+
+func (c *Config) getMemberMaxId() int {
+	var maxId int
+	for _, member := range c.Members {
+		if member.Id > maxId {
+			maxId = member.Id
+		}
+	}
+	return maxId
 }
 
 func (c *Config) AddMember(member *Member) {
+	member.Id = c.getMemberMaxId()
 	c.Members = append(c.Members, member)
 }
 
