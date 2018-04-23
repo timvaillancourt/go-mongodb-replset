9bb75e69439f194673a78daf5551f53777fce491
Tim Vaillancourt <tim@timvaillancourt.com>
2018-04-23 23:25:42 +0200

    Use go-mongodb-fixtures for testing

--- status/status_test.go
+++ status/status_test.go
@@ -3,26 +3,44 @@ package status
 import (
 	"testing"
 
-	"github.com/timvaillancourt/go-mongodb-replset/test"
+	"github.com/timvaillancourt/go-mongodb-replset/fixtures"
 )
 
-const fixturesDir = "../test/fixtures"
-
-func getStatusFixture(version string) (*Status, error) {
+func getStatusFixture(t *testing.T, version string) *Status {
 	s := &Status{}
-	err := test.LoadFixture(fixturesDir, version, statusCommand, s)
-	return s, err
+	err := fixtures.LoadFixture(version, statusCommand, s)
+	if err != nil {
+		t.Errorf("Error loading fixture for %s: %s", version, err)
+	}
+	return s
 }
 
 func TestGetSelf(t *testing.T) {
-	for _, version := range test.FixtureVersions(fixturesDir) {
-		s, err := getStatusFixture(version)
-		t.Logf("Testing .GetSelf() for %s", version)
-		if err != nil {
-			t.Errorf("Error loading fixture for %s: %s", version, err)
-		}
+	for _, version := range fixtures.FixtureVersions() {
+		s := getStatusFixture(t, version)
+		t.Logf("Testing status.GetSelf() for %s", version)
 		if s.GetSelf() == nil {
-			t.Errorf("Error for %s: .GetSelf() returned nil!", version)
+			t.Errorf("Error for %s: status.GetSelf() returned nil!", version)
+		}
+	}
+}
+
+func TestGetMemberId0(t *testing.T) {
+	for _, version := range fixtures.FixtureVersions() {
+		s := getStatusFixture(t, version)
+		t.Logf("Testing status.GetMemberId(0) for %s", version)
+		if s.GetMemberId(0) == nil {
+			t.Errorf("Error for %s: status.GetMemberId(0) returned nil!", version)
+		}
+	}
+}
+
+func TestPrimary(t *testing.T) {
+	for _, version := range fixtures.FixtureVersions() {
+		s := getStatusFixture(t, version)
+		t.Logf("Testing status.Primary() for %s", version)
+		if s.Primary() == nil {
+			t.Errorf("Error for %s: status.Primary() returned nil!", version)
 		}
 	}
 }
