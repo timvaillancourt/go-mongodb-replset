1b0ce6bc9929b3433b075c38465b89f9cef0e141
Tim Vaillancourt <tim@timvaillancourt.com>
2018-04-26 21:44:29 +0200

    Add more tags funcs

--- config/member_test.go
+++ config/member_test.go
@@ -33,12 +33,27 @@ func TestNewMember(t *testing.T) {
 	}
 }
 
-func TestReplsetTagsHasTag(t *testing.T) {
-	if !testMember.Tags.HasTag("test", "123456") {
-		t.Errorf("member.Tags.HasTag() returned false for %v=%v", "test", "123456")
+func TestReplsetTagsHasKey(t *testing.T) {
+	if !testMember.Tags.HasKey("test") {
+		t.Errorf("member.Tags.HasKey() returned false for %v", "test")
 	}
-	if testMember.Tags.HasTag("does not", "exist") {
-		t.Error("member.Tags.HasTag() returned true for missing key")
+	if testMember.Tags.HasKey("does not exist") {
+		t.Error("member.Tags.HasKey() returned true for missing key")
+	}
+}
+
+func TestReplsetTagsHasMatch(t *testing.T) {
+	if !testMember.Tags.HasMatch("test", "123456") {
+		t.Errorf("member.Tags.HasMatch() returned false for %v=%v", "test", "123456")
+	}
+	if testMember.Tags.HasMatch("test", "1234567") {
+		t.Error("member.Tags.HasMatch() returned true for missing match")
+	}
+
+}
+func TestReplsetTagsGetTagValue(t *testing.T) {
+	if testMember.Tags.GetTagValue("test") != "123456" {
+		t.Errorf("member.Tags.GetTagValue(\"test\") returned false for %v=%v", "test", "123456")
 	}
 }
 
